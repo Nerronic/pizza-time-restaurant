301a17d944332709c303d56fde471ebabe7465ff
Ty <tyrellcarlton@gmail.com>
2024-03-22 14:44:50 -0700

    first commit

---
+++ src/components/header/header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Header() {
+  return (
+    <>
+  
+      <div className="header"> 
+      <img src={ '/logo.png'} alt="myLogo" className="logo" /> 
+ </div>
+      
+
+    </>
+  );
+}
+
+export default Header;
