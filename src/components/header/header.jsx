596ef320027ec6071fdbc2b217512341f875d266
Ty <tyrellcarlton@gmail.com>
2024-03-23 18:49:06 -0700

    the header and css placement

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -6,6 +6,7 @@ function Header() {
   
       <div className="header"> 
       <img src={ '/logo.png'} alt="myLogo" className="logo" /> 
+      <h2 className="title-slogan" > Abduct your Hunger</h2>
  </div>
       
 
