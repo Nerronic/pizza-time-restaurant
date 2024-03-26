cb369696bc1095ac010e7c2a366ccf97142e8db5
Ty <tyrellcarlton@gmail.com>
2024-03-26 09:48:48 -0700

    did some resizing of images, linked nav section, need to style

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import './App.css'
 import Hero from "./components/hero";
 import Header from './components/header'
-
+import Nav from "./components/nav"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <Nav/>
      <Header/>
     <Hero/>
     </>
